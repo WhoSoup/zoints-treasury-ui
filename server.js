6b2b06b3a3294446df24ae8a59c5eeeec5edcf5c
WhoSoup <who.soup@gmail.com>
2021-12-03 11:15:40 +0100

    working devnet

---
+++ server.js
@@ -0,0 +1,9 @@
+import * as express from 'express';
+const app = express.default();
+const port = 8080;
+
+app.use('/', express.static('public'));
+
+app.listen(port, async () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
