1e01e21f402a6725ac04da1b0740c015f7e357e2
WhoSoup <who.soup@gmail.com>
2021-12-03 11:23:04 +0100

    move index to main

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import * as express from 'express';
 const app = express.default();
 const port = 8080;
 
-app.use('/', express.static('public'));
+app.use('/', express.static('./'));
 
 app.listen(port, async () => {
     console.log(`Server started at http://localhost:${port}`);
